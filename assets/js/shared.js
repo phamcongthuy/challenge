1e1920a5afd18015ba96025907fe26bf308b79b8
Jim Thoburn <jim@jimthoburn.com>
2018-02-10 16:01:22 -0800

    Rename style sheet, and move JS into a separate file.

---
+++ assets/js/shared.js
@@ -0,0 +1,140 @@
+(function() {
+
+  if (!document.querySelector || !document.body.addEventListener || !document.body.getBoundingClientRect || window.scrollY === undefined) return;
+
+  var images = document.querySelectorAll('figure img');
+
+
+    // As the view is scrolled up or down
+      // Move the image up or down based on scroll position
+
+      // https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element#11396681
+      // var rect = image.getBoundingClientRect();
+      // console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+  var positions = [];
+
+  function calculatePositions() {
+    for (var index = 0; index < images.length; index++) {
+      positions.push(images[index].getBoundingClientRect().top);
+    }
+  }
+
+  // function isVisible(element, elementTop) {
+  //   var elementBottom = elementTop + element.offsetHeight;
+  //   var windowBottom  = window.scrollY  + window.innerHeight;
+
+  //   // If the top edge of the window is greater than the top edge of the target
+  //   if ((elementTop > window.scrollY && elementTop < windowBottom) || 
+  //       (elementBottom > window.scrollY && elementBottom < windowBottom)) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
+  function isVisible(element) {
+    var elementTop = element.getBoundingClientRect().top;
+    var elementBottom = elementTop + element.offsetHeight;
+    if (elementTop - window.innerHeight < 0 && elementBottom > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function update() {
+    for (var index = 0; index < images.length; index++) {
+
+      if (isVisible(images[index], positions[index])) {
+        var scrollPositionRelativeToImage = images[index].getBoundingClientRect().top;
+
+        var scrollPercentage = scrollPositionRelativeToImage / images[index].offsetHeight;
+        images[index].style.objectPosition = (50 + (scrollPercentage * 2)) + '% ' + (50 + (scrollPercentage * 10)) + '%';
+      }
+
+    }
+  }
+
+  var doUpdate = true;
+  window.addEventListener('scroll', function() {
+    if (doUpdate) {
+      requestAnimationFrame(update);
+      doUpdate = false;
+      setTimeout(function() {
+        doUpdate = true;
+      }, 100);
+    }
+  });
+
+  var timer;
+  window.addEventListener('resize', function(e) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(function() {
+      calculatePositions();
+      update();
+    }, 100); // Wait for the resize to finish before recalculating
+  });
+
+  window.addEventListener('load', function(e) {
+    calculatePositions();
+    update();
+  });
+
+  calculatePositions();
+  update();
+
+  window.__updateImages = function() {
+    calculatePositions();
+    update();
+  }
+
+})();
+
+
+(function() {
+
+  var navLink = document.querySelector('.nav-link a');
+  if (navLink) {
+
+    // https://stackoverflow.com/questions/1034621/get-the-current-url-with-javascript
+    var currentPage = window.location.href.toString().split(window.location.host)[1];
+
+    navLink.setAttribute('href', '/navigation?previous_page=' + currentPage);
+
+    // navLink.addEventListener('click', function(e) {
+    //   document.body.classList.toggle('has-active-menu');
+    //   e.preventDefault();
+    // }, false);
+  }
+
+})();
+
+(function() {
+
+  /* Do we have the features we need? */
+  if (!document.querySelector || !window.addEventListener || !document.body.classList) return;
+
+  function updateScrollPosition(e) {
+    /* OPTIONAL: Add a class name of “header-not-visible” to the header,
+                 including the navigation, is not visible. */
+    if (window.scrollY >= 300) {
+      document.body.classList.add('header-not-visible');
+    } else {
+      document.body.classList.remove('header-not-visible');
+    }
+  }
+
+  updateScrollPosition();
+
+  (function() {
+    var throttle;
+    window.addEventListener('scroll', function() {
+      if (throttle) clearTimeout(throttle);
+      throttle = setTimeout(updateScrollPosition, 10);
+    }, false);
+  })();
+})();
+
+
+
